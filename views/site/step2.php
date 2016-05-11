8ad6e9c9ba1c467921b910c90277104592d277ba
tienping <petwesley@gmail.com>
2016-05-12 01:51:06 +0800

    HERMO-TEST-2: Checkout Page
    * Add shipping fee in the price details
    * add decimal point for price listing
    * change ship to area input to dropdown
    * update shipment fee on change of ship to area dropdown
    
    ** TODO: add handling to total price, tired now.. continue next time **

--- views/site/step2.php
+++ views/site/step2.php
@@ -28,16 +28,28 @@ $this->params['breadcrumbs'][] = $this->title;
 								echo $brand->name; 
 							?></h4>
 							<div class="checkout-name"><?php echo $model->name; ?></div>
-							<div class="checkout-selling-price"><?php echo $model->symbol.' '.$model->selling_price; ?></div>
-							<div class="checkout-retail-price"><?php echo $model->symbol.' '.$model->retail_price; ?></div>
+							<div class="checkout-selling-price">
+								<?php echo $model->symbol.' '.number_format((float)$model->selling_price, 2, ".", "")." X ".$quantity; ?>
+							</div>
+							<div class="checkout-retail-price"><?php echo $model->symbol.' '.number_format((float)$model->retail_price, 2, ".", ""); ?></div>
+							<div class="checkout-shipping-fee-div">
+								<span class="checkout-shipping-fee-label">
+									Shipping Fee:
+								</span>
+								<span class="checkout-shipping-fee-amount pending">
+									(Pending ship area)
+								</span>
+							</div>
+							
 							<hr class="black-line">
+							
 							<div class="checkout-total-price-div">
 								<span class="checkout-total-price-label">
-									Total Price
+									Total Price:
 								</span>
 								<span class="checkout-total-price-amount">
 									<?php 
-										echo $model->symbol.' '.($model->selling_price * $quantity);
+										echo $model->symbol.' '.number_format((float)($model->selling_price * $quantity), 2, '.', '');
 									?>
 								</span>
 							</div>
@@ -49,15 +61,64 @@ $this->params['breadcrumbs'][] = $this->title;
 			<div class="checkout-details-div">
 				<div class="shipment-details-div">
 					<span class="shipment-label">
-						Ship to area
+						Ship to area:
 					</span>
 					<span class="shipment-destination">
-						<input type="text">
+						<select class="shipment-destination-select">
+							<options>
+								<option>Country</option>
+								<option>Malaysia</option>
+								<option>Singapore</option>
+								<option>Brunei</option>
+								<option>Others</option>
+							</options>
+						</select>
 					</span>
 				</div>
+				<script>
+					var shippingSelect = document.querySelector(".shipment-destination-select");
+					var shippingFeeAmount = document.querySelector(".checkout-shipping-fee-amount");
+					
+					if (shippingSelect && shippingFeeAmount) {
+						var changingShippingArea = function() {
+							var shippingDestOpt = shippingSelect.options[shippingSelect.selectedIndex];
+							if (shippingDestOpt && shippingDestOpt.value) {
+								var shippingDestValue = shippingDestOpt.value;
+								var quantity = <?php echo $quantity ?>;
+								var totalPrice = <?php echo number_format((float)($model->selling_price * $quantity), 2, '.', '') ?>;
+								
+								switch(shippingDestOpt.value) {
+									case "Country":
+										shippingFeeAmount.innerHTML = "(Pending ship area)";
+										alert("Please choose a destination.");
+										break;
+									case "Malaysia":
+										shippingFeeAmount.innerHTML = (quantity >= 2 || totalPrice >= 150) ? "FREE" : "RM 10";
+										break;
+									case "Singapore":
+										shippingFeeAmount.innerHTML = (totalPrice >= 300) ? "FREE" : "RM 20";
+										break;
+									case "Brunei":
+										shippingFeeAmount.innerHTML = (totalPrice >= 300) ? "FREE" : "RM 25";
+										break;
+									default:
+										shippingFeeAmount.innerHTML = "(Pending ship area)";
+										alert("Only support delivery to Malaysia, Singapore and Brunei at the momemt.");
+										break;
+								}
+								
+								// TODO: add handling to total price, tired now.. continue next time
+							}
+						};
+						
+						shippingSelect.onchange = changingShippingArea;
+					} else {
+						console.warn("shippingSelect not found...");
+					}
+				</script>
 				<div class="promocode-details-div">
 					<span class="promocode-label">
-						Promotion code
+						Promotion code:
 					</span>
 					<span class="promocode-string">
 						<input type="text">
